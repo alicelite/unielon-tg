fb054fa84e40e2605f807b2ec2067ce87f3df1d0
hebirong <hebirong@igengmei.com>
2024-08-07 16:50:29 +0800

    fix(test): test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import AppRoutes from "./routes/index";
+// import AppRoutes from "./routes/index";
+// import "./assets/style/var.less"
+// function App() {
+//   return (
+//     <AppRoutes />
+//   );
+// }
+// export default App;
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/main/WelcomeScreen";
 import "./assets/style/var.less"
 function App() {
   return (
-    <AppRoutes />
+    <Routes>
+        <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import WebApp from '@twa-dev/sdk'
 import { Buffer } from 'buffer';
+import { BrowserRouter } from 'react-router-dom';
 import { ActionComponentProvider } from '@/components/ActionComponent';
 window.Buffer = Buffer;
 window.global = window.globalThis;
@@ -10,7 +11,9 @@ WebApp.ready();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
      <ActionComponentProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
      </ActionComponentProvider>
   </React.StrictMode>,
 )
