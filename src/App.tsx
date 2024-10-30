603fec4c494f919442f5df3d6979e0f31c6c38f7
alicelite <alicelite@proton.me>
2024-10-30 13:18:23 +0800

    fix(

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useIntegration } from '@telegram-apps/react-router-integration';
-import { initNavigator } from '@telegram-apps/sdk-react';
-import { useEffect, useMemo, useState } from 'react';
-import {
-  Route,
-  Router,
-  Routes,
-  useNavigate,
-} from 'react-router-dom';
-import routesConfig from './routes/index';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import routesConfig from "./routes/index";
 import "./assets/style/var.less";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 const generateRoutes = (routes: any[]) => {
   return routes.map((route) => {
     const { path, element: Element, children } = route;
@@ -29,40 +23,53 @@ const generateRoutes = (routes: any[]) => {
   });
 };
 
+export type DisplayDataRow = { title: string } & (
+  | { type: "link"; value?: string }
+  | { value: React.ReactNode }
+);
 const RedirectHandler = () => {
   const navigate = useNavigate();
   const [checked, setChecked] = useState(false);
-
+  const startParam = undefined
+  console.log("startParam", startParam);
   useEffect(() => {
+    console.log("checked", checked)
     if (!checked) {
-      const password = localStorage.getItem('password');
-      if (password) {
-        navigate('/account/unlock');
+      const password = localStorage.getItem("password");
+      if (startParam) {
+        console.log("startParam---", startParam);
+        if(password) {
+          navigate(`/app_connect/${startParam}`, {
+            replace: true,
+          });
+        } else {
+          console.log("welcome---1");
+          navigate("/welcome");
+        }
       } else {
-        navigate('/');
+        if (password) {
+          console.log("account/unlock");
+          navigate("/account/unlock");
+        } else {
+          console.log("welcome---2");
+          navigate("/welcome");
+        }
       }
       setChecked(true);
     }
-  }, [navigate, checked]);
+  }, [navigate, checked, startParam]);
 
   return null;
 };
 
 function App() {
-  const navigator = useMemo(() => initNavigator('app-navigation-state'), []);
-  const [location, reactNavigator] = useIntegration(navigator);
-  useEffect(() => {
-    navigator.attach();
-    return () => navigator.detach();
-  }, [navigator]);
-
   return (
-    <Router location={location} navigator={reactNavigator}>
-      <RedirectHandler />
-      <Routes>
-        {generateRoutes(routesConfig)}
-      </Routes>
-    </Router>
+    <>
+      <BrowserRouter>
+        <RedirectHandler />
+        <Routes>{generateRoutes(routesConfig)}</Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
